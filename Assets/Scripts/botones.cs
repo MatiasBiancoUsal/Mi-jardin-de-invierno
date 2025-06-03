bc75c2ebe4afe389549f2cba4b81da30e75bee65
mguadalupecenteno <mariaguadalupe.centeno@usal.edu.ar>
2025-06-03 13:15:57 -0300

    Hoy hice canvas de botón "Jugar" y script para que cuando se clickee botón de "Jugar" redirija a pantalla principal. También escena nueva de inventario con canvas de botones "semillas" y "añadido" para que cuando se toque el botón de semillas, aparezca el nuevo objeto y cambie a "añadido"

---
+++ Assets/Scripts/botones.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+
+public class botones : MonoBehaviour
+{
+    
+public Button semillas;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+       public void EscenaJuego()
+
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    public void CargarNivel(string NombreNivel)
+
+    {
+        SceneManager.LoadScene(NombreNivel);
+    }
+
+    
+    public void BotonAñadido()
+    {
+        semillas.gameObject.SetActive(false);
+    }
+
+}
