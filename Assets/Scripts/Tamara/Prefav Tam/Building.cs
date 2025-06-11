60fa5b5b12f6bd096a5200bc599cf57df04b1b53
tamaradelosheros <tamara.delosheros@usal.edu.ar>
2025-06-10 21:28:39 -0300

    Nuevo sistema de ubicar objetos
    Probando un nuevo método para la ubicación de objetos.

---
+++ Assets/Scripts/Tamara/Prefav Tam/Building.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Building : MonoBehaviour
+{
+    public bool Placed {  get; private set; }
+    public BoundsInt area;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    #region Building Methods
+    #endregion
+    
+}
