966576adc144c0fd7a5df86d2cb083d740d10933
macamustone <m.espolsinmustone@usal.edu.ar>
2025-06-18 17:25:19 -0300

    Dirigir botones
    Cree el scipt y configure el boton de inventario Otoño, en el invernadero para que se pueda visualizar

---
+++ Assets/Scripts/MACA/IraInventario.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class IraInventario : MonoBehaviour
+{
+
+    public void IrAEscena()
+    {
+        SceneManager.LoadScene("Inventario Otoño");
+    }
+}
