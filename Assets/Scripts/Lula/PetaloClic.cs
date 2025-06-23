5d4bc12592091886236fc431ee1a3a7da7211ae0
EstevezMLucia <estevez.marialucia@usal.edu.ar>
2025-06-23 18:31:49 -0300

    Petalos
    Se agregó a la planta base un pétalo, su animación y se modificaron los scripts para que al momento en que la planta llegue a su felicidad máxima aparezca el pétalo y al clicearlo se sume 1 punto al contador de pétalos.

---
+++ Assets/Scripts/Lula/PetaloClic.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class PetaloClic : MonoBehaviour
+{
+    void OnMouseDown()
+    {
+        if (ContadorPetalo.instancia != null)
+        {
+            ContadorPetalo.instancia.SumarPetalo();
+        }
+        else
+        {
+            Debug.LogWarning("No se encontró el ContadorPetalos en la escena.");
+        }
+
+        gameObject.SetActive(false);
+    }
+}
