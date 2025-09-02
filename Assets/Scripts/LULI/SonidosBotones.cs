8df161723a9f139162c263122a429e2d7ec2cd6a
mguadalupecenteno <mariaguadalupe.centeno@usal.edu.ar>
2025-09-02 17:46:47 -0300

    Sonidos Planta Verano2

--- Assets/Scripts/LULI/SonidosBotones.cs
+++ Assets/Scripts/LULI/SonidosBotones.cs
@@ -1,43 +1,20 @@
 using UnityEngine;
 
-[RequireComponent(typeof(Animator))]
 public class SonidosBotones : MonoBehaviour
 {
-    private Animator animator;
-    private string animacionAnterior = "";
-
-    void Awake()
+     public void SonidoRegar()
     {
-        animator = GetComponent<Animator>();
+        AudioManager.instance.ReproducirSonido("regar");
     }
 
-    void Update()
+    public void SonidoAbonar()
     {
-        // Obtenemos el estado actual del Animator (capa 0)
-        AnimatorStateInfo stateInfo = animator.GetCurrentAnimatorStateInfo(0);
+        AudioManager.instance.ReproducirSonido("abonar");
+    }
 
-        // Detectamos si cambió de animación
-        if (stateInfo.IsName("Regar") && animacionAnterior != "Regar")
-        {
-            AudioManager.instance?.ReproducirSonido("regar");
-            animacionAnterior = "Regar";
-        }
-        else if (stateInfo.IsName("Plantar") && animacionAnterior != "Plantar")
-        {
-            AudioManager.instance?.ReproducirSonido("plantar");
-            animacionAnterior = "Plantar";
-        }
-        else if (stateInfo.IsName("Abono") && animacionAnterior != "Abono")
-        {
-            AudioManager.instance?.ReproducirSonido("abonar");
-            animacionAnterior = "Abono";
-        }
-        else if (!stateInfo.IsName("Regar") && 
-                 !stateInfo.IsName("Plantar") && 
-                 !stateInfo.IsName("Abono"))
-        {
-            // Si no estamos en ninguna animación especial, reseteamos
-            animacionAnterior = "";
-        }
+    public void SonidoPlantar()
+    {
+        AudioManager.instance.ReproducirSonido("plantar");
     }
 }
+
