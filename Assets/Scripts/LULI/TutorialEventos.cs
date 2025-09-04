80e90331784a018117641543926f5a8a8c2778f2
mguadalupecenteno <mariaguadalupe.centeno@usal.edu.ar>
2025-09-03 21:04:13 -0300

    Tutorial pasos

--- Assets/Scripts/LULI/TutorialEventos.cs
+++ Assets/Scripts/LULI/TutorialEventos.cs
@@ -35,7 +35,7 @@ public class TutorialEventos : MonoBehaviour
             Debug.LogWarning("TutorialManager no está asignado en TutorialEventos");
         }
     }
-    
+
     public void PlantadoTerminado()
     {
         if (tutorialManager != null)
@@ -43,7 +43,22 @@ public class TutorialEventos : MonoBehaviour
             // Llama a RegistrarAccion y le pasas el nombre de la acción que se completó.
             // Asegúrate de que este string coincida con el que pusiste en el Inspector de Unity
             // en el campo 'Accion Esperada' del paso correspondiente.
-            tutorialManager.RegistrarAccion("plantar"); 
+            tutorialManager.RegistrarAccion("plantar");
+        }
+        else
+        {
+            Debug.LogWarning("TutorialManager no está asignado en TutorialEventos");
+        }
+    }
+    
+    public void PetaloTerminado()
+    {
+        if (tutorialManager != null)
+        {
+            // Llama a RegistrarAccion y le pasas el nombre de la acción que se completó.
+            // Asegúrate de que este string coincida con el que pusiste en el Inspector de Unity
+            // en el campo 'Accion Esperada' del paso correspondiente.
+            tutorialManager.RegistrarAccion("petalo"); 
         }
         else
         {
